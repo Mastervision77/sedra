11d80edfdcc33bb60d0b5dac29e102a9b573ec96
Eslam-Salah74 <eslamsalah20003000@gmail.com>
2025-12-18 10:40:25 +0200

    edit link

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -66,7 +66,7 @@ $setting = App\Models\Settings::first();
     <div id="mobile-menu"
         class="absolute z-50 px-4 h-screen bg-primary w-full lg:hidden hidden py-4 border-t border-gold/20">
         <div class="flex flex-col gap-8">
-            <a href="{{route('mediacenter')}}"
+            <a href=""
                 class="text-accent hover:text-gold transition-colors duration-300 font-lora text-sm uppercase tracking-wide">Media
                 Center</a>
             <a href="{{route('about')}}"
